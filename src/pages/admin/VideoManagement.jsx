76867339db0a38ed8fbc565c2f911fc11a5dbb93
Wavei-Haruna <wavharun2@mail.com>
2024-07-25 17:05:38 +0000

    Remove sensitive info

--- src/pages/admin/VideoManagement.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function VideoManagement() {
-  return (
-    <div>VideoManagement</div>
-  )
-}
--- src/pages/admin/courseManagement.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function courseManagement() {
-  return (
-    <div>courseManagement</div>
-  )
-}
